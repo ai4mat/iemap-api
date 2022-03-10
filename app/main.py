a8871ae9572bd5331a8b7ac927659d41c02635ee
SergioEanX <sergio.ferlito@enea.it>
2022-03-10 11:23:47 +0100

    added env variable for HOST and PORT

--- app/main.py
+++ app/main.py
@@ -1,4 +1,14 @@
 import uvicorn
+from app.log_config import logging_config
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values(".env.shared"),  # load shared development variables
+    **dotenv_values(".env.secret"),  # load sensitive variables
+    # **os.environ,  # override loaded values with environment variables
+}
+HOST = config["HOST"]
+PORT = int(config["PORT"]) if config["PORT"].isdigit() else 8000
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server.app:app", host=HOST, port=PORT, reload=True)
