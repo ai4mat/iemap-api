6a0e0a095e983338a9531d8d48db88d4f6362388
SergioEanX <sergio.ferlito@enea.it>
2022-03-17 09:27:08 +0100

    graphql test example code

---
+++ app/server/mutations.py
@@ -0,0 +1,35 @@
+from ariadne import ObjectType, convert_kwargs_to_snake_case
+
+from server.store import users, messages, queue
+
+mutation = ObjectType("Mutation")
+
+
+@mutation.field("createMessage")
+@convert_kwargs_to_snake_case
+async def resolve_create_message(obj, info, content, sender_id, recipient_id):
+    try:
+        message = {
+            "content": content,
+            "sender_id": sender_id,
+            "recipient_id": recipient_id
+        }
+        messages.append(message)
+        await queue.put(message)  # add the message to the queue
+        return {"success": True, "message": message}
+    except Exception as error:
+        return {"success": False, "errors": [str(error)]}
+
+
+@mutation.field("createUser")
+@convert_kwargs_to_snake_case
+async def resolve_create_user(obj, info, username):
+    try:
+        if not users.get(username):
+            user = {"user_id": len(users) + 1, "username": username}
+            users[username] = user
+            return {"success": True, "user": user}
+        return {"success": False, "errors": ["Username is taken"]}
+
+    except Exception as error:
+        return {"success": False, "errors": [str(error)]}
---
+++ app/server/store.py
@@ -0,0 +1,13 @@
+import asyncio
+
+messages = []
+users = {}
+
+queue = asyncio.Queue()
+
+
+def create_user(username):
+    if not users.get(username):
+        user = {"user_id": len(users) + 1, "username": username}
+        users[username] = user
+        return {"success": True, "user": user}
