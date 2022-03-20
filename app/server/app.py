8d9cd261816af3cf615e4809867e335f29e4f483
SergioEanX <sergio.ferlito@enea.it>
2022-03-20 08:33:25 +0100

    added files route and  version decorator

--- app/server/app.py
+++ app/server/app.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from fastapi import FastAPI, Request
+from fastapi_versioning import VersionedFastAPI, version
 # from humanize import naturaltime
 from server.routes.items import router as ItemRouter
-
+from server.routes.files import router as FilesRouter
 from fastapi.middleware.cors import CORSMiddleware
-
 
 from ariadne import make_executable_schema, load_schema_from_path, \
     snake_case_fallback_resolvers
@@ -36,16 +36,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.add_route('/graphql', graphql_app)
+# app.add_route('/graphql', graphql_app)
 app.include_router(ItemRouter, tags=["Items"], prefix="/api")
+
+app.include_router(FilesRouter, tags=["Files"], prefix="/api")
 
 
 @app.get("/api", tags=["Root"])
+# @version(1, 0)
 async def read_root():
     return {"message": "Welcome to the IEMAP API"}
 
 
-@app.api_route("/{path_name:path}", methods=["GET"])
+@app.get("/api", tags=["Root latest"])
+# @version(1, 1)
+async def read_root():
+    return {"message": "Welcome to the IEMAP API LATEST"}
+
+
+@app.api_route("/v1_0/{path_name:path}", methods=["GET"])
 async def catch_all(request: Request, path_name: str):
     return {
         "request_method":
@@ -57,3 +66,8 @@ async def catch_all(request: Request, path_name: str):
         #  naturaltime(date=datetime.now())
         datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     }
+
+
+# APPLY VERSIONING
+app = VersionedFastAPI(app, default_version=(1, 0), enable_latest=True)
+app.add_route('/graphql', graphql_app)
