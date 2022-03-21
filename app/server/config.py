300fd4d3d55aad8cc67fd1a4c843659851d3269c
SergioEanX <sergio.ferlito@enea.it>
2022-03-21 17:06:04 +0100

    added mongodb config from .env.shared

--- app/server/config.py
+++ app/server/config.py
@@ -1,6 +1,15 @@
 import os
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values(".env.shared"),  # load shared development variables
+    **dotenv_values(".env.secret"),  # load sensitive variables
+    # **os.environ,  # override loaded values with environment variables
+}
+
 
 class Config(object):
     # mongodb
-    mongo_db = os.getenv('MONGO_DATABASE')
-    mongo_uri = os.getenv('MONGO_URI')
+    mongo_db = config["MONGO_DATABASE"]
+    mongo_uri = config['MONGO_URI']
+    mongo_coll = config['MONGO_COLLECTION']
