d354ceed5e8998d3e2a74626d181c5026644e10a
SergioEanX <sergio.ferlito@enea.it>
2022-05-01 20:09:18 +0200

    NEW MAIN start this

--- app/main.py
+++ app/main.py
@@ -1,14 +1,61 @@
-import uvicorn
-from log_config import logging_config
-from dotenv import dotenv_values
+from datetime import datetime
+from logging.config import dictConfig
+from fastapi import FastAPI, Request
+from starlette.exceptions import HTTPException
+from starlette.middleware.cors import CORSMiddleware
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-config = {
-    **dotenv_values(".env.shared"),  # load shared development variables
-    **dotenv_values(".env.secret"),  # load sensitive variables
-    # **os.environ,  # override loaded values with environment variables
-}
-HOST = config["HOST"]
-PORT = int(config["PORT"]) if config["PORT"].isdigit() else 8000
+# import routes from a specific version
+from api.api_v1.api import router as api_router
+
+# import configuration
+from core.config import Config
+from core.errors import http_422_error_handler, http_error_handler
+from db.mongodb_utils import close_mongo_connection, connect_to_mongo
+
+# loads logging configuration
+from core.log_config import logging_config
+
+# configure logging using a dictionary from core.log_config
+dictConfig(logging_config)
+
+# define FASTAPI application
+app = FastAPI(title=Config.app_name)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=Config.allowed_hosts,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
+
+app.add_exception_handler(HTTPException, http_error_handler)
+app.add_exception_handler(HTTP_422_UNPROCESSABLE_ENTITY, http_422_error_handler)
+
+
+# actualy add routes
+app.include_router(api_router, prefix=Config.api_v1_str)
+
+
+# CATCH ALL ROUTE IT NEEDS TO BE LAST
+# @app.api_route("/{path_name:path}", methods=["GET"])
+# async def catch_all(request: Request, path_name: str):
+#     return {
+#         "request_method": request.method,
+#         "path_name": path_name,
+#         "message": "Reply from IEMAP API at " +
+#         #  naturaltime(date=datetime.now())
+#         datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+#     }
+
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host=HOST, port=PORT, reload=True)
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, log_level="info", reload=True)
