4c77223bb1056d9d6c9435b67b1eba934cc8a248
SergioEanX <sergio.ferlito@enea.it>
2022-05-08 20:08:37 +0200

    checkauth and jwt routes added

--- app/api/api_v1/endpoints/health.py
+++ app/api/api_v1/endpoints/health.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Response, status, Request
+from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException
 
 
-from core.jwt import get_current_user_authorizer
+# from core.jwt import get_current_user_authorizer
+from core.auth import JWTBearer, signJWT
+from models.user import UserBase, User
 from crud.health import checkDB
 from db.mongodb import AsyncIOMotorClient, get_database
 
@@ -26,3 +29,29 @@ async def check_health(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         detail="Something went wrong!!",
     )
+
+
+# http://0.0.0.0:8001/api/v1/health/checkauth
+@router.get(
+    "/health/checkauth",
+    dependencies=[Depends(JWTBearer())],
+    tags=["JWT"],
+)
+async def check_auth(request: Request) -> dict:
+    # get current user from JWT paylaod using Request state
+    payload = request.state.payload
+    return {"status": "You are authenticated!", "user": payload["user_id"]}
+
+
+# http://0.0.0.0:8001/api/v1/health/jwt
+@router.post("/health/jwt", tags=["JWT"], status_code=status.HTTP_200_OK)
+async def get_jwt(user: UserBase) -> User:
+    if user.email == "ai4mat@enea.it":
+        return signJWT(user.email)
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content={"error": "Unauthorized user!!"},
+    )
+
+
+# https://stackoverflow.com/questions/68827065/how-to-get-return-values-form-fastapi-global-dependencies
