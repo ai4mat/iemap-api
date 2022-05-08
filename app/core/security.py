656b15037b6643e9472523e29d78fd025cb97720
SergioEanX <sergio.ferlito@enea.it>
2022-05-08 20:11:04 +0200

    added comments

--- app/core/security.py
+++ app/core/security.py
@@ -14,3 +14,12 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+# The secret key is used for encoding and decoding JWT strings.
+# The algorithm value on the other hand is the type of algorithm used in the encoding process.
+# TO GENERATE A SECRET KEY
+# import os
+# import binascii
+# binascii.hexlify(os.urandom(24))
+# b'deff1952d59f883ece260e8683fed21ab0ad9a53323eca4f'
