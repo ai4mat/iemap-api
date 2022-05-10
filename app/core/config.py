4f1ad6ca4183aef612f347053f90170299ff405c
SergioEanX <sergio.ferlito@enea.it>
2022-05-10 19:42:55 +0200

    set relative path for .env (find_dotenv fails)

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv, dotenv_values
 from starlette.datastructures import CommaSeparatedStrings, Secret
 
 # from databases import DatabaseURL
@@ -10,14 +10,15 @@ API_V1_STR = "/api"
 JWT_TOKEN_PREFIX = "Token"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # one week
 
-load_dotenv(".env")
+# load_dotenv(find_dotenv(raise_error_if_not_found=True))
 
-import os
-from dotenv import dotenv_values
+path_dot_env = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../.env")
 
 config = {
-    **dotenv_values(".env"),  # load shared development variables
-    **dotenv_values(".env.secret"),  # load sensitive variables
+    **dotenv_values(path_dot_env),  # load shared development variables
+    # find_dotenv(raise_error_if_not_found=True, usecwd=False)
+    # ),  # load shared development variables
+    # **dotenv_values(".env.secret"),  # load sensitive variables
     # **os.environ,  # override loaded values with environment variables
 }
 
@@ -32,11 +33,12 @@ class Config(object):
     min_conn = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
     jwt_secret_key = config["JWT_SECRET_KEY"]
     jwt_algorithm = config["JWT_ALGORITHM"]
+    jwt_token_prefix = "Token"
+    access_token_expire_minutes = 60 * 24 * 7  # one week
     app_name = config["APP_NAME"]
     allowed_hosts = CommaSeparatedStrings(config.get("ALLOWED_HOSTS", "*"))
     api_v1_str = config["API_V1_STR"]
-    jwt_token_prefix = "Token"
-    access_token_expire_minutes = 60 * 24 * 7  # one week
+    files_dir = config["FILESDIR"]
 
 
 # MONGODB_URL = os.getenv("MONGODB_URL", "")  # deploying without docker-compose
