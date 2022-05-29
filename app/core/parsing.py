c68a15019099b78042b31317059bee8532b00aed
SergioEanX <sergio.ferlito@enea.it>
2022-05-29 09:49:40 +0200

    updated

--- app/core/parsing.py
+++ app/core/parsing.py
@@ -10,29 +10,41 @@ def parse_cif(cif_file):
     """Parse a CIF file and return a pymatgen structure object."""
     parser = CifParser(cif_file)
     structure = parser.get_structures()[0]
-    distinct_species = [
-        str(x).replace("Element", "") for x in list(set(structure.species))
-    ]
+    # distinct_species = [
+    #     str(x).replace("Element", "") for x in list(set(structure.species))
+    # ]
     lattice = structure.lattice
-    return structure, distinct_species, lattice
+    elements = structure.symbol_set
+    # composition = structure.composition
+    formula = structure.formula
+    return formula, elements, lattice
 
+
+if __name__ == "__main__":
+
+    import os
+
+    cif_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "../../example.cif"
+    )
+    cif_data = parse_cif(cif_file)
+    print(cif_data)
 
 # import os
 
-# fp = os.path.join(
+# fp_out = os.path.join(
 #     os.path.dirname(os.path.realpath(__file__)), "../../uploaded/aiida.out"
 # )
 
 # fp_in = os.path.join(
 #     os.path.dirname(os.path.realpath(__file__)), "../../uploaded/aiida.in"
 # )
-# # with open(fp_in) as f:
-# #     contents = f.read()
-# # try:
-# aiida_out = PWOutput(fp)
-# print(aiida_out)
-# aiida_in = PWInput.from_file(fp_in)
+# with open(fp_in) as f:
+#     contents = f.read()
+# try:
+#     aiida_out = PWOutput(fp_out)
+#     print(aiida_out)
+#     aiida_in = PWInput.from_file(fp_in)
 # except Exception as e:
 #     print(e)
 #     print("Error parsing PWSCF input file.")
-# r1 = pwscf.PWInput.(contents.splitlines())
