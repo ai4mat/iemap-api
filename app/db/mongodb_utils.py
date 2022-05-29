e61b5a7d6b3c704a3ef70071dee9e0ba4093ab95
SergioEanX <sergio.ferlito@enea.it>
2022-05-29 09:50:44 +0200

    conditional auth on premise

--- app/db/mongodb_utils.py
+++ app/db/mongodb_utils.py
@@ -1,3 +1,8 @@
+from beanie import init_beanie
+from beanie import PydanticObjectId
+
+from fastapi_users.db import BeanieBaseUser, BeanieUserDatabase
+
 from datetime import datetime
 import logging
 
@@ -5,6 +10,14 @@ logger = logging.getLogger("ai4mat")
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import Config
 from db.mongodb import db
+
+
+class User(BeanieBaseUser[PydanticObjectId]):
+    pass
+
+
+async def get_user_db():
+    yield BeanieUserDatabase(User)
 
 
 async def connect_to_mongo():
@@ -16,6 +29,17 @@ async def connect_to_mongo():
     logger.info(
         f"Connection succesfully established at {datetime.now().strftime('%Y-%B-%d %H:%M:%S')}."
     )
+    if Config.enable_onpremise_auth:
+        collection_users = "user_db_jwt"
+        await init_beanie(
+            database=db.client[collection_users],
+            document_models=[
+                User,
+            ],
+        )
+        logger.info(
+            f"Succesfully initialize On-Premise AUTH using collection {collection_users}."
+        )
 
 
 async def close_mongo_connection():
