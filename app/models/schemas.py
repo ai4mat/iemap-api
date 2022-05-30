c9f54d46661f6dcb89c9ec896e14c3dd68f89caa
SergioEanX <sergio.ferlito@enea.it>
2022-05-30 09:21:22 +0200

    added affiliation:Optional[str]=None

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,14 +1,23 @@
 from beanie import PydanticObjectId
 from fastapi_users import schemas
+from typing import Optional
 
 
+# ANY ADDITIONAL FIELD TO BE PERSISTED IN THE UserAuth COLLECTION in MongoDB
+# NEEDS TO BE DEFINED HERE ASS WELL AS IN THE USERAUTH MODEL in mongodb_utils.py
 class UserRead(schemas.BaseUser[PydanticObjectId]):
+    # added to support affiliation info
+    affiliation: Optional[str] = None
     pass
 
 
 class UserCreate(schemas.BaseUserCreate):
+    # added to support affiliation info
+    affiliation: Optional[str] = None
     pass
 
 
 class UserUpdate(schemas.BaseUserUpdate):
+    # added to support affiliation info
+    affiliation: Optional[str] = None
     pass
