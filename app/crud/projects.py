636ad854f2071d9b041106be557b50318b3fcafb
SergioEanX <sergio.ferlito@enea.it>
2022-06-15 11:44:43 +0200

    added docstring to add_project_file

--- app/crud/projects.py
+++ app/crud/projects.py
@@ -91,8 +91,23 @@ async def add_project_file(
     fileExt: str,
     fileName: str,
 ):
-    """Function to add file hash to PROJECT
-    The document to update is identified by the document's id, and file's name and extention.
+    """Function to add file to PROJECT
+    (using filne name hash and original extention)
+    The document to update is identified by the mdocuent's id, and file's name and extention.
+
+    Parameters:
+    -----------
+        conn: AsyncIOMotorClient - Motor MongoDB client connection
+        id: str - MongoDB document's id (ObjectId) to update
+        fileHash: str - file hash
+        fileSize: str - file size
+        fileExt: str - file extention
+        fileName: str - file name
+
+    Returns:
+    --------
+        modified_count: int - number of modified documents
+
     """
     #  {"_id":ObjectId("62752dd88856514dab27dd8e")},
     # {$set:{"process.properties.$[elem].hash":"hash-2"}},{arrayFilters:[{$and:[{"elem.name":"H2o"},{"elem.type":"2D"}]}]}
@@ -113,7 +128,9 @@ async def add_project_file(
             {"$and": [{"elem.name": fileName}, {"elem.extention": fileExt}]}
         ],
     )
-    return result_update.modified_count
+    num_records_updated = result_update.modified_count
+
+    return num_records_updated
 
 
 async def find_project_file_by_hash(conn: AsyncIOMotorClient, file_hash: str, id: str):
