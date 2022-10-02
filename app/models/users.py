fc4d90a5038861869f48b5064672a5a480ef7ced
SergioEanX <sergio.ferlito@enea.it>
2022-10-02 12:19:08 +0200

    updated sending email verify account

--- app/models/users.py
+++ app/models/users.py
@@ -1,5 +1,4 @@
 from typing import Optional
-
 from beanie import PydanticObjectId
 from fastapi import Depends, Request
 from starlette.datastructures import URL
@@ -12,12 +11,14 @@ from fastapi_users.authentication import (
 )
 from fastapi_users.db import BeanieUserDatabase, ObjectIDIDMixin
 from db.mongodb_utils import UserAuth, get_user_db
-from core.smtp_email import Email
+from core.smtp_email import readVerifyMailTemplate, send_mail_async
 from core.config import Config
 
 from fastapi_users.manager import BaseUserManager
 from fastapi_users import exceptions
 from fastapi_users.jwt import generate_jwt
+from pathlib import Path
+
 
 SECRET = Config.secrete_on_premise_auth
 
@@ -52,19 +53,25 @@ class UserManager(ObjectIDIDMixin, BaseUserManager[UserAuth, PydanticObjectId]):
         # retrieve requested url to use for link to embend in email sent to user
         strBaseRequest = str(request.url).split("auth/register")[0]
         strEndpointVerifyByEmail = "auth/verify-email/"
-        strEndpointToEmbedinEmail = strBaseRequest + strEndpointVerifyByEmail
-        em = Email()
+        strLinkVerifyEmail = strBaseRequest + strEndpointVerifyByEmail + token
+
         # send email to user to verify his/her email
         # eventually add other email to list to notify an administrator
-        em.send_verify_email([user.email], strEndpointToEmbedinEmail, token)
+        pathVerifyEmail = (
+            "./app/templates/mail_template.html"
+            if not "app" in str(Path.cwd()).split("/")
+            else "./templates/mail_template.html"
+        )
+        textMail = await readVerifyMailTemplate(pathVerifyEmail, strLinkVerifyEmail)
+        await send_mail_async(
+            [user.email], "Finish registration to IEMAP REST API service", textMail
+        )
 
     async def on_after_forgot_password(
         self, user: UserAuth, token: str, request: Optional[Request] = None
     ):
 
         print(f"User {user.id} has forgot their password. Reset token: {token}")
-        em = Email()
-        em.send_verify_email([user.email], token)
 
     async def on_after_request_verify(
         self, user: UserAuth, token: str, request: Optional[Request] = None
@@ -72,13 +79,16 @@ class UserManager(ObjectIDIDMixin, BaseUserManager[UserAuth, PydanticObjectId]):
         # retrieve requested url to use for link to embend in email sent to user
         strBaseRequest = str(request.url).split("auth/request-verify-token")[0]
         strEndpointVerifyByEmail = "auth/verify-email/"
-        em = Email()
-        # send email to user to verify his/her email
-        # eventually add other email to list to notify an administrator
-        em.send_verify_email(
-            [user.email], strBaseRequest + strEndpointVerifyByEmail, token
+        strLinkVerifyEmail = strBaseRequest + strEndpointVerifyByEmail + token
+        pathVerifyEmail = (
+            "./app/templates/mail_template.html"
+            if not "app" in str(Path.cwd()).split("/")
+            else "./templates/mail_template.html"
         )
-        # print(f"Verification requested for user {user.id}. Verification token: {token}")
+        textMail = await readVerifyMailTemplate(pathVerifyEmail, strLinkVerifyEmail)
+        await send_mail_async(
+            [user.email], "Finish registration to IEMAP REST API service", textMail
+        )
 
 
 async def get_user_manager(user_db: BeanieUserDatabase = Depends(get_user_db)):
