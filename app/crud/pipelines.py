178b762ac64b4e758c86831bd126007be47d1576
SergioEanX <sergio.ferlito@enea.it>
2022-11-14 17:09:08 +0100

    added get_user_projects_base_info

--- app/crud/pipelines.py
+++ app/crud/pipelines.py
@@ -9,3 +9,26 @@ def get_properties_files(affiliation: str, projectName: str) -> dict:
         },
     ]
     return pipeline
+
+
+def get_user_projects_base_info(user_email: str, affiliation: str) -> dict:
+    pipeline = [
+        {
+            "$match": {
+                "provenance.email": user_email,
+                "provenance.affiliation": affiliation,
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "id": "$_id",
+                "project_name": "$project.name",
+                "date_creation": "$provenance.createdAt",
+                "iemap_id": "$iemap_id",
+                "experiment": "$process.isExperiment",
+                "material": "$material.formula",
+            }
+        },
+    ]
+    return pipeline
