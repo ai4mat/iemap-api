f47ddb1fa8a3a55f10d436fa530b90bc0466a14b
SergioEanX <sergio.ferlito@enea.it>
2023-03-09 11:39:01 +0100

    added stats routes

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -4,8 +4,8 @@ from api.api_v1.endpoints.authentication import router as auth_router
 from api.api_v1.endpoints.health import router as health_router
 from api.api_v1.endpoints.fileshandling import router as files_router
 from api.api_v1.endpoints.project import router as projects_router
-
 from api.api_v1.endpoints.user_projects import router as user_proj_info_router
+from api.api_v1.endpoints.stats import router as stats
 
 router = APIRouter()
 router.include_router(auth_router)
@@ -13,3 +13,4 @@ router.include_router(health_router)
 router.include_router(files_router)
 router.include_router(projects_router)
 router.include_router(user_proj_info_router)
+router.include_router(stats)
