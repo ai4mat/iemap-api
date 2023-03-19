08ff713d11724ed0aec6c990f8747245b657ac26
SergioEanX <sergio.ferlito@enea.it>
2023-03-19 12:44:05 +0100

    added iemap_formulas_elements

--- app/api/api_v1/endpoints/stats.py
+++ app/api/api_v1/endpoints/stats.py
@@ -12,7 +12,7 @@ from db.mongodb_utils import UserAuth
 # from core.jwt import get_current_user_authorizer
 from core.auth import JWTBearer, signJWT  # , decodeJWT
 from models.user import UserBase, User
-from crud.stats import project_stat, project_stat_user
+from crud.stats import project_stat, project_stat_user, iemap_formulas_elements
 from db.mongodb import AsyncIOMotorClient, get_database
 
 # Use fastapi_users instance as Dependence Injection into routes
@@ -74,16 +74,65 @@ async def stats_project(
 ):
 
     # dictionary result aggreation pipeline
-    result = await project_stat_user(db, user.email)
+    result = await iemap_formulas_elements(db)
     if type(result) is dict:
         return {"data": result}
-    # return a JSON resposne as
+    # return a JSON response as
     #     {
     #     "data": {
     #         "total": 45, # total number of projects in DB
     #         "totalByUser": 42, # total number of projects for the current l user
     #         "totalByUserWithFile": 24, # total number of projects for the current user with at least one file
     #         "totalByUserCountFiles": 74 # total number of files for the current user
+    #     }
+    # }
+
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Something went wrong!!",
+    )
+
+
+# http://0.0.0.0:8001/api/v1/stats_iemap
+# GET DISTICNT FORMULAS AND ELEMENTS in IEMAP DB (as whole)
+# with theri counts
+@router.get(
+    "/stats_iemap",
+    tags=["stats"],
+    status_code=status.HTTP_200_OK,
+    summary="Distinc Materials and Elements ind IEMAP DB (with counts)",
+    description="This route gets all distinct materials' formulas and elements in IEMAP DB (with counts)",
+)
+async def stats_project(
+    db: AsyncIOMotorClient = Depends(get_database),
+    response: Response = Response(status_code=status.HTTP_200_OK),
+):
+
+    # dictionary result aggreation pipeline
+    result = await iemap_formulas_elements(db)
+    if type(result) is dict:
+        return {"data": result}
+    # return a JSON response as
+    #         {
+    #     "data": {
+    #         "formulas": [
+    #             "C6H12",
+    #             "GAZ2058",
+    #             "Mn6Na12Ni6O24",
+    #             "C6H12O6",
+    #             "CONO2"
+    #         ],
+    #         "unique_elements": [
+    #             "C",
+    #             "H",
+    #             "Mn",
+    #             "N",
+    #             "Na",
+    #             "Ni",
+    #             "O"
+    #         ],
+    #         "n_formulas": 5,
+    #         "n_elements": 7
     #     }
     # }
 
